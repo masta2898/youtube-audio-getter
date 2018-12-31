7bfc1242b36f8eb487a08a06ffe909cf205c39e6
sehat <sehat1137@yandex.ru>
2018-12-31 17:19:00 +0300

    Нихуя за день не сделал

---
+++ src/getAudio.py
@@ -0,0 +1,15 @@
+from pytube import YouTube
+
+class GetAudio:
+
+    def __init__(self, cut_len=10):
+        self.cut_len = cut_len
+
+    def get_youtube_audio(self, link):
+        video = YouTube(link)
+
+        video.streams.filter(only_audio=True).first().download()
+
+
+
+
